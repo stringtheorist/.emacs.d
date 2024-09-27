b29cfa288d7080cf6c4bf5c942e314f77b9e7ddd
stringtheorist <aftab26989@gmail.com>
2024-09-28 00:45:45 +0200

    add line to remove bell.

--- init.el
+++ init.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
  '(fido-vertical-mode t)
  '(menu-bar-mode nil)
  '(package-archive-priorities '(("gnu" . 100) ("nongnu" . 50) ("melpa" . 0)))
@@ -10,7 +12,7 @@
    '(("gnu" . "https://elpa.gnu.org/packages/")
      ("nongnu" . "https://elpa.nongnu.org/nongnu/")
      ("melpa" . "https://melpa.org/packages/")))
- '(package-selected-packages '(julia-mode auctex eat markdown-mode magit))
+ '(package-selected-packages '(dracula-theme julia-mode auctex eat markdown-mode magit))
  '(pixel-scroll-mode t)
  '(pixel-scroll-precision-mode t)
  '(tab-always-indent 'complete)
@@ -47,3 +49,5 @@
 (setq-default ispell-program-name "aspell")
 (setq backup-directory-alist `(("." . "~/.emacs_backups_aftab")))
 
+;; remove the bell
+(setq ring-bell-function 'ignore)
